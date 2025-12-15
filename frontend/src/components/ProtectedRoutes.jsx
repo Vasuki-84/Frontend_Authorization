90f42f51a4026a973aebd73c4931b304b2a7ea01
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-15 20:10:11 +0530

    added protected routes

---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ Children }) => {
+  const token = localStorage.getItem("token"); // get the token from backend ans store it in local storage
+  return token ? Children : <Navigate to="/" />; // if user has token navigate to children else navigate to keep the user in login page
+};
+export default ProtectedRoutes;
